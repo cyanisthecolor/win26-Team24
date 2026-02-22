03bfd5fae69e3e37171a1fb9632410883dc46342
Jonathan Rodriguez <jrrod@stanford.edu>
2026-02-21 16:42:19 -0800

    Learned how to use Google Calendar API to add events to a user's calendar. I created another email account (win26team24@gmail.com), which we can use for testing purposes.

--- jrrod/calendar_oauth.py
+++ jrrod/calendar_oauth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from datetime import datetime, timezone, timedelta 
+from typing import TypedDict, NotRequired
 import os
 
 from google_auth_oauthlib.flow import InstalledAppFlow
@@ -8,10 +9,10 @@ from google.oauth2.credentials import Credentials
 from googleapiclient.discovery import build
 from google.auth.transport.requests import Request
 
-SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"]
+SCOPES = ["https://www.googleapis.com/auth/calendar"]
 CALENDARS = ["primary"]
 
-def get_creds():
+def get_calendar():
     creds = None;
 
     if os.path.exists("token.json"):
@@ -30,31 +31,73 @@ def get_creds():
         with open("token.json", "w") as token:
             token.write(creds.to_json());
 
-    return creds;
+    return build("calendar", "v3", credentials=creds);
 
-def get_this_month(creds):
+def get_range(calendar, start: datetime, end: datetime):
     events = [];
 
-    service = build("calendar", "v3", credentials=creds);
-    now = datetime.now(timezone.utc).isoformat();
-    then = (datetime.now(timezone.utc)+timedelta(days=30)).isoformat();
-
-    for calendar in CALENDARS:
-        events += service.events().list(
-            calendarId=calendar,
-            timeMin=now,
-            timeMax=then,
+    for id in CALENDARS:
+        events += calendar.events().list(
+            calendarId=id,
+            timeMin=start.isoformat(),
+            timeMax=end.isoformat(),
             singleEvents=True,
             orderBy="startTime"
         ).execute().get("items", []);
 
     return events;
 
-def main():
-    creds = get_creds();
+def get_this_month(calendar):
+    now = datetime.now(timezone.utc);
+    return get_range(calendar, now, now+timedelta(days=30));
 
-    for event in get_this_month(creds):
+class Event(TypedDict):
+    start: datetime
+    end: datetime
+    summary: str
+    description: str
+    location: NotRequired[str]
+    attendees: NotRequired[list[Person]]
+
+class Person(TypedDict):
+    email: str
+    optional: NotRequired[bool]
+
+def make_event(calendar, calendarId: str, event: Event):
+    created_event = calendar.events().insert(
+        calendarId=calendarId,
+        body=event | {
+            "start": {
+                "dateTime": event["start"].isoformat(),
+                "timeZone": str(event["start"].tzinfo), 
+            },
+            "end": {
+                "dateTime": event["end"].isoformat(),
+                "timeZone": str(event["end"].tzinfo), 
+            },
+        },
+    ).execute();
+
+    print("Event created:");
+    print(created_event.get("htmlLink"));
+
+    return created_event;
+
+def main():
+    calendar = get_calendar();
+
+    for event in get_this_month(calendar):
         start = event["start"].get("dateTime", event["start"].get("date"));
         print(start, event.get("summary", "(no title)"));
+
+    start_time = datetime.now(timezone.utc) + timedelta(hours=1);
+    end_time = start_time + timedelta(hours=3);
+    event = {
+        "summary": "Grinding",
+        "description": "Not much time left",
+        "start": start_time,
+        "end": end_time,
+    };
+    make_event(calendar, CALENDARS[0], event);
 
 if __name__ == "__main__": main()
