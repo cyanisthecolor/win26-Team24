4dbb7c599b99dfff61ed33f06422e6c3a5775c92
Jonathan Rodriguez <jrrod@stanford.edu>
2026-02-21 16:42:19 -0800

    Learned how to use Google Calendar API to add events to a user's calendar. I created another email account (win26team24@gmail.com), which we can use for testing purposes.

--- jrrod/main.py
+++ jrrod/main.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
-from calendar_oauth import get_this_month, get_creds;
+from calendar_oauth import get_this_month, get_calendar;
 from ask import ask;
 
 def main():
-    events = get_this_month(get_creds());
+    events = get_this_month(get_calendar());
     response = ask(events,
         "Summarize the events in the user's Google calendar. Emphasize events that are closer to the current time. Focus, also, on events that seem most important. You do not necessarily need to mention every repeated event, but do mention tests, meetings, deadlines, etc. Be as precise, concise, and efficient as possible. There is no point in summarizing if the length is the same as the original content. Minimize token usage."
     );
